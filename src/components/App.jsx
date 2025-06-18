2cc20a15f2010726d67c7ef42a327247960842a9
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-06-18 05:12:03 +0200

    ajout de protectedroute sur la page user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "../pages/home/home";
-import Login from "../pages/login/Login";
-import User from "../pages/user/User";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./Layout"
+import Home from "../pages/home/home"
+import Login from "../pages/login/Login"
+import User from "../pages/user/User"
+import ProtectedRoute from "./ProtectedRoute"
 
 const router = createBrowserRouter([
   {
@@ -18,18 +19,22 @@ const router = createBrowserRouter([
       },
       {
         path: "/profile",
-        element: <User />,
+        element: (
+          <ProtectedRoute>
+            <User />
+          </ProtectedRoute>
+        ),
       },
     ],
   },
-]);
+])
 
 function App() {
   return (
     <div>
       <RouterProvider router={router} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
